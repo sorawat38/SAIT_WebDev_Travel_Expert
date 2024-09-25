30dacd993c74cfbcf60b7a2083ac99186dace6a3
sorawat38 <st.sorawat38@gmail.com>
2024-09-24 23:17:37 -0600

    [feat] add part 3 and 4

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 
 // main binding event listener
-document.getElementById("registerForm").addEventListener("submit", confirmSubmission)
+if (window.location.pathname === '/index.html') {
+    let travelImgDiv = document.getElementById("travelImages");
+    images.forEach((image) => {
+        let imgElement = document.createElement('img');
+        let divElement = document.createElement('div');
+        imgElement.height = 200;
+        imgElement.width = 500;
+        imgElement.src = image;
+        imgElement.classList.add('image');
+        divElement.append(imgElement);
+        travelImgDiv.appendChild(divElement);
+    });
+
+    let imgClass = document.querySelectorAll('.image')
+    for (let i = 0; i < imgClass.length; i++) {
+        let descriptionNode = document.createElement('p');
+        descriptionNode.innerHTML = descriptions[i];
+        imgClass[i].addEventListener('mouseover', function () {
+            imgClass[i].parentElement.appendChild(descriptionNode)
+        })
+        imgClass[i].addEventListener('mouseout', function () {
+            imgClass[i].parentElement.removeChild(descriptionNode);
+        })
+    }
+
+} else if (window.location.pathname === '/register.html') {
+    document.getElementById("registerForm").addEventListener("submit", confirmSubmission)
+} else if (window.location.pathname === '/contact.html') {
+
+}
 
 // function
 function confirmSubmission(e) {
@@ -14,3 +43,4 @@ function confirmSubmission(e) {
     }
     alert(text);
 }
+
